b241ac8e5c5fddb3b151e7f92a051df1b766282f
First Fractal <firstfractal512@gmail.com>
2023-01-23 01:31:01 -0600

    made adreanline toggable

--- BossAdreanlineMode.cs
+++ BossAdreanlineMode.cs
@@ -1,9 +1,57 @@
 using Terraria;
+using Terraria.ID;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
 
 namespace BossAdreanlineMode
 {
 	public class BossAdreanlineMode : Mod
 	{
 	}
+
+    public class BossAdreanlineSystem : ModSystem
+    {
+        public bool adreanline = false;
+        public int counter = 0;
+        public int adreanlineCounter = 0;
+        public int adreanlineCounterMax = 20;
+        public void toggleAdreanline()
+        {
+            counter++;
+            if (counter >= 60)
+            {
+                adreanlineCounter++;
+                Main.NewText(adreanlineCounter.ToString(), Color.SeaShell);
+                counter = 0;
+            }
+
+            if (adreanlineCounter >= adreanlineCounterMax)
+            {
+                adreanline = !adreanline;
+                Main.NewText("The adrealine is at " + adreanline.ToString(), Color.Red);
+                adreanlineCounter = 0;
+            }
+        }
+
+
+        //spawn in the npcs for the server
+        public override void PostUpdateEverything()
+        {
+            if (Main.netMode == NetmodeID.Server)
+            {
+                toggleAdreanline();
+            }
+            base.PostUpdateEverything();
+        }
+
+        //spawn in the npcs for singleplayer
+        public override void PostUpdateWorld()
+        {
+            if (Main.netMode == NetmodeID.SinglePlayer)
+            {
+                toggleAdreanline();
+            }
+            base.PostUpdateWorld();
+        }
+    }
 }
--- GlobalModBoss.cs
+++ GlobalModBoss.cs
@@ -1,35 +1,37 @@
 ﻿using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
+using Microsoft.Xna.Framework;
 
 namespace BossAdreanlineMode
 {
     public class GlobalModBoss : GlobalNPC
     {
-        static int cooldownMax = 1200;
-        static int cooldown = cooldownMax;
-        static bool adrenaline = false;
         static bool speed = false;
         static int[] BossParts = { NPCID.SlimeSpiked, NPCID.ServantofCthulhu, NPCID.EaterofWorldsHead, NPCID.EaterofWorldsBody, NPCID.EaterofWorldsTail, NPCID.Creeper, NPCID.SkeletronHand, NPCID.SkeletronHead, NPCID.WallofFleshEye, NPCID.TheDestroyer, NPCID.TheDestroyerBody, NPCID.TheDestroyerTail, NPCID.Probe, NPCID.PrimeCannon, NPCID.PrimeLaser, NPCID.PrimeSaw, NPCID.PrimeVice, NPCID.PlanterasHook, NPCID.PlanterasTentacle, NPCID.GolemFistLeft, NPCID.GolemFistRight, NPCID.GolemHead, NPCID.GolemHeadFree, NPCID.CultistTablet, NPCID.CultistBossClone, NPCID.LunarTowerNebula, NPCID.LunarTowerSolar, NPCID.LunarTowerVortex, NPCID.LunarTowerStardust, NPCID.MoonLordCore, NPCID.MoonLordHand, NPCID.MoonLordHead, NPCID.MoonLordFreeEye, NPCID.MoonLordLeechBlob };
 
         public override void PostAI(NPC npc)
         {
-            if (npc.boss)
+            bool adreanline = ModContent.GetInstance<BossAdreanlineSystem>().adreanline; 
+            if (adreanline)
             {
-                speed = true;
-            }
-
-            foreach(int bossPart in BossParts)
-            {
-                if (npc.type == bossPart)
+                if (npc.boss)
                 {
                     speed = true;
                 }
-            }
 
-            if (speed)
-            {
-                npc.position += npc.velocity * 1.5f;
+                foreach(int bossPart in BossParts)
+                {
+                    if (npc.type == bossPart)
+                    {
+                        speed = true;
+                    }
+                }
+
+                if (speed)
+                {
+                    npc.position += npc.velocity * 1.5f;
+                }
             }
             base.PostAI(npc);
         }
